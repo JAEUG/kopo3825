f61283f975671f7d60fd0c21d24fb661c3b52c3e
JAEUG <161946591+JAEUG@users.noreply.github.com>
2025-04-07 09:49:14 +0900

    Update 2025-04-03 (목) C숙제03(자리수의합구하는프로그램).c

--- AI금융소프트웨어_C실습(숙제)/2025-04-03 (목) C숙제03(자리수의합구하는프로그램).c
+++ AI금융소프트웨어_C실습(숙제)/2025-04-03 (목) C숙제03(자리수의합구하는프로그램).c
@@ -8,7 +8,7 @@ void main() {
 
 	int num100 = abs(num) / 100;		// 백의자리 값
 	int num10 = abs((num / 10)) % 10;	// 십의자리 값
-	int num1 = abs(num) % 10;			// 일의자리 값
+	int num1 = abs(num) % 10;		// 일의자리 값
 
 	printf("자리수의 합: %d", num100 + num10 + num1);
 
