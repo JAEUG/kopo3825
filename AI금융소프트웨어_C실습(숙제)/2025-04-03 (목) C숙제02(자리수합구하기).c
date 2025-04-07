5773bfe03c658e87d425735ea71bb7fc8b31d93c
JAEUG <161946591+JAEUG@users.noreply.github.com>
2025-04-07 10:11:38 +0900

    Update 2025-04-03 (목) C숙제02(자리수합구하기).c

--- AI금융소프트웨어_C실습(숙제)/2025-04-03 (목) C숙제02(자리수합구하기).c
+++ AI금융소프트웨어_C실습(숙제)/2025-04-03 (목) C숙제02(자리수합구하기).c
@@ -5,13 +5,13 @@ void main() {
 	
 	int num;
 	scanf("%d", &num);
-    if (num <= 999 && num >= -999) {
-    	int num100 = abs(num) / 100;		// 백의자리 값
-    	int num10 = abs((num / 10)) % 10;	// 십의자리 값
-    	int num1 = abs(num) % 10;		// 일의자리 값
+    	if (num <= 999 && num >= -999) {
+	    	int num100 = abs(num) / 100;		// 백의자리 값
+	    	int num10 = abs((num / 10)) % 10;	// 십의자리 값
+	    	int num1 = abs(num) % 10;		// 일의자리 값
 		printf("자리수의 합: %d", num100 + num10 + num1);
-    } else {
-        printf("정수(최대3자리)를 입력하세요");
-    }// if
+	} else {
+	        printf("정수(최대3자리)를 입력하세요");
+	}// if
     
 }// main
