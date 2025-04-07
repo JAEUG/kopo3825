42bd9c00816cd22e4ebd7760ebaf0e150488d4cc
JAEUG <161946591+JAEUG@users.noreply.github.com>
2025-04-07 16:40:36 +0900

    Add files via upload

---
+++ AI금융소프트웨어_C실습(숙제)/2025-04-07 (월) C숙제01(특이한수열탐색기).c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+// 특이한 수열 탐색기
+void main() {
+	int m, n;
+
+	int dummy = scanf("%d %d", &m, &n);
+
+	if (n >= m && n > 0 && m > 0) {
+		for (int i = m; i <= n; i++) {
+			if (i % 7 == 0 || i % 10 == 7 || (i / 10) % 10 == 7 || i / 100 == 7 || i / 1000 == 7 || i / 10000 == 7) {
+				continue;
+			}
+			printf("[%d]  ", i);
+			if (i % 5 == 0) {
+				break;
+			}
+		}// for
+	} else {
+		printf("m과 n의 입력값은 0이상어야 하며, n의 입력값은 m의 입력값보다 크거나 같아야합니다.");
+	}
+}// main
