19c1bce0b6ff818e600550a4a241577dabec5366
JAEUG <161946591+JAEUG@users.noreply.github.com>
2025-04-08 10:42:59 +0900

    Add files via upload

---
+++ AI금융소프트웨어_C실습(숙제)/2025-04-08 (화) C숙제02(1부터N까지출력하기).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() {
+    
+    int n;
+    scanf("%d", &n);
+    int i = 1;
+    
+    while (i <= n) {
+        printf("%d ", i);
+        i++;
+    }// while 
+
+}// main
