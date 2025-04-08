bb907c3efaec29491767399129bf12c882156c60
JAEUG <161946591+JAEUG@users.noreply.github.com>
2025-04-08 09:58:24 +0900

    Add files via upload

---
+++ AI금융소프트웨어_C실습(숙제)/2025-04-08 (화) C숙제01(역직각삼각형 출력).c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main() {
+	int inputNum;
+
+	int dummy = scanf("%d", &inputNum);
+	
+	int numberOfStars = inputNum;
+
+	for (int i = 0; i < inputNum; i++) {
+		for (int j = numberOfStars; j > 0; j--) {
+			printf("*");
+		}// for
+		printf("\n");
+		numberOfStars--;
+	}// for
+
+}// main
