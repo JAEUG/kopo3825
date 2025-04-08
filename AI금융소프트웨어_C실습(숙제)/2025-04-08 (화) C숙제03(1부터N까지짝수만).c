3de2a45de1cf0a1381875d187322095c63f2b87e
JAEUG <161946591+JAEUG@users.noreply.github.com>
2025-04-08 11:44:10 +0900

    Update 2025-04-08 (화) C숙제03(1부터N까지짝수만).c

--- AI금융소프트웨어_C실습(숙제)/2025-04-08 (화) C숙제03(1부터N까지짝수만).c
+++ AI금융소프트웨어_C실습(숙제)/2025-04-08 (화) C숙제03(1부터N까지짝수만).c
@@ -5,13 +5,13 @@ void main() {
     scanf("%d", &n);
     int i = 1;
     
-    while (i <= n) {
+    while (i <= n) { // for문처럼 증감이 루프마다 자동으로 되는게 아니기 때문에( for문도 증감 부분을 따로 빼서 쓸 수 있긴하더라... ) 
         if (i % 2 == 1) {
-            i++;
+            i++;    // continue가 있을때는 조건이 True일때 바로 루프의 시작지점으로 돌아가기 때문에 continue를 하기 전에 i를 증가시켜줘야 한다!
             continue;
         }
         printf("%d ", i);
-        i++;
+        i++;        // 위에 continue가 있으면 이 부분(i를 증가시키는)이 실행이 안된다
     }// while 
 
 }// main
