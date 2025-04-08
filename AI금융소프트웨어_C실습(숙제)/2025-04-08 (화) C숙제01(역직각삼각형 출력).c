0631b89682c9f7d94db225ac0c408778f3fde95f
JAEUG <161946591+JAEUG@users.noreply.github.com>
2025-04-08 10:00:07 +0900

    Update 2025-04-08 (화) C숙제01(역직각삼각형 출력).c

--- AI금융소프트웨어_C실습(숙제)/2025-04-08 (화) C숙제01(역직각삼각형 출력).c
+++ AI금융소프트웨어_C실습(숙제)/2025-04-08 (화) C숙제01(역직각삼각형 출력).c
@@ -2,9 +2,8 @@
 
 void main() {
 	int inputNum;
-	int numberOfStars = inputNum;
-	
 	int dummy = scanf("%d", &inputNum);
+	int numberOfStars = inputNum;
 	
 	for (int i = 0; i < inputNum; i++) {
 		for (int j = numberOfStars; j > 0; j--) {
