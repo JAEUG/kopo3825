cfe8e5a48a27c05dab68fbb1d501710476532025
JAEUG <161946591+JAEUG@users.noreply.github.com>
2025-04-09 08:56:40 +0900

    Update 2025-04-08 (화) C숙제02(1부터N까지출력하기).c

--- AI금융소프트웨어_C실습(숙제)/2025-04-08 (화) C숙제02(1부터N까지출력하기).c
+++ AI금융소프트웨어_C실습(숙제)/2025-04-08 (화) C숙제02(1부터N까지출력하기).c
@@ -2,12 +2,14 @@
 
 void main() {
     int n;
-    scanf("%d", &n);
+    scanf_s("%d", &n);
     int i = 1;
-    
-    while (i <= n) {
-        printf("%d ", i);
-        i++;
-    }// while 
-
+    if (n >= 1) {
+        while (i <= n) {
+            printf("%d ", i);
+            i++;
+        }// while 
+    } else {
+        printf("1 이상의 정수를 입력하세요");
+    }
 }// main
