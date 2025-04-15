3ccce09348387446aa819036efa9e977bac537c7
JAEUG <161946591+JAEUG@users.noreply.github.com>
2025-04-15 14:03:16 +0900

    Update 2025-04-07 (월) C숙제02(삽입정렬구현remaster).c

--- AI금융소프트웨어_C실습(숙제)/2025-04-07 (월) C숙제02(삽입정렬구현remaster).c
+++ AI금융소프트웨어_C실습(숙제)/2025-04-07 (월) C숙제02(삽입정렬구현remaster).c
@@ -7,7 +7,7 @@ void main() {
     int j;
     for (int i = 1; i < arrSize; i++) {
         tmp = arr[i]; // 비교기준 1번 index부터
-        for (j = i; j > 0 && tmp < arr[j - 1]; j--) { // 비교되는 대상은 비교기준 - 1부터 0까지
+        for (j = i; j > 0 && tmp < arr[j - 1]; j--) { // 비교되는 대상은 (비교기준 - 1)부터 0까지
             arr[j] = arr[j - 1]; // 한칸씩 뒤로
         }// for
         arr[j] = tmp; // 알맞은 위치에 비교기준 값을 삽입
